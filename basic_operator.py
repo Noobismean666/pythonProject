e441f3bf7c57f91b9d9fd424fa8b34f242d64773
Noobismean666 <793664403@qq.com>
2022-06-15 00:50:48 +0800

    Initial commit

---
+++ basic_operator.py
@@ -0,0 +1,39 @@
+result1 = 10 + 3
+print(result1)
+print(10 + 3) # 加法
+result2 = 100 - 93
+print(result2) # 减法
+result3 = 88 * 66
+print(result3) #乘法
+result4 = 88 / 4
+print(result4) #浮点数除法
+result5 = 100 // 3
+print(result5) #除法取整
+result6 = 100 % 3
+print(result6) #取余数（取模）
+result7 = 2 ** 4
+print(result7) # 2的4次方（幂）
+
+print(result7-result6)
+result8 = (result7+result6) * result1 + 3000
+print(result8)
+
+
+# 计算m、n两个数的平均值和m的平方乘以n的平方
+m = 10
+n = 5
+
+# 变量m的值加3，n的值加5
+m = m + 3
+n = n + 5
+
+# 求m和n的平均值
+averageResult = (m + n) / 2
+
+# 求m的平方乘以n的平方
+productResult = (m**2) * (n**2)
+
+print("m和n的平均值：", averageResult)
+print("m的平方乘以n的平方值：", productResult)
+
+
