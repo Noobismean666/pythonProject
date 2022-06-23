99e44ba3a149ce8072e426cdef9fceda50eed8ca
Noobismean666 <793664403@qq.com>
2022-06-23 21:26:31 +0800

    6-23

---
+++ loop/sample1.py
@@ -0,0 +1,12 @@
+# while循环的使用方法
+# print("Python is the best language")
+#1.定义循环的执行条件
+#2.编写要被执行的循环代码
+#3.编写修改执行条件代码
+i = 0
+while i < 5:
+    print("Python is the best language")
+    i = i + 1 #自身加1
+
+
+
