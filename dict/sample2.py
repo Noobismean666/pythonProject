b0e64800be12771ccff839087c1f8787cf9ec754
Noobismean666 <793664403@qq.com>
2022-07-14 20:17:43 +0800

    7-14

---
+++ dict/sample2.py
@@ -0,0 +1,26 @@
+# 字典的取值操作
+employee = {'name':'王峰','sex':"男",
+         'hiredate':'1997-10-20','grade':'A',
+         'job':'销售','salary':1000,
+         'welfare':100
+            }
+# 字典的取值
+name = employee['name']
+print(name)
+salary = employee['salary']
+print(salary)
+
+print(employee.get('job'))
+print(employee.get('dept','其它部门'))
+
+# in 成员运算符
+print('name' not in employee)
+print('dept' in employee)
+
+# 遍历字典
+for key in employee:
+    v = employee[key]
+    print(v)
+
+for key , value in employee.items():
+    print(key,value)
